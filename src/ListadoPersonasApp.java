e21415783453547c69379bbca887de01e5eab4dc
Alejandro <yeisoonc159@gmail.com>
2024-06-13 19:50:15 -0500

    Codigo completo

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -11,13 +11,18 @@ public class ListadoPersonasApp {
 
         //Menu
         var salir = false;
-        while (!salir){
+        while (!salir) {
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(consola, personas);
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error: " + e.getMessage());
+            }
             System.out.println();
         }
     }
 
-    private static void mostrarMenu(){
+    private static void mostrarMenu() {
         //Mostrar las opciones
         System.out.println("""
                 **** Listado Personas App ****
@@ -26,6 +31,48 @@ public class ListadoPersonasApp {
                 3. Salir
                 """);
         System.out.print("Proporciona la opcion: ");
-
     }
+
+    private static boolean ejecutarOperacion(Scanner consola, List<Persona> personas) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        //Revisamos la opcion proporcionada
+        switch (opcion) {
+            case 1: { //Agregar persona a la lista
+                System.out.println("Proporciona el nombre: ");
+                var nombre = consola.nextLine();
+
+                System.out.println("Proporciona el telefono: ");
+                var tel = consola.nextLine();
+
+                System.out.println("Proporciona el email: ");
+                var email = consola.nextLine();
+
+                //Crear el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                // lo agreagamos a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + "elementos");
+                break;
+            }// Fin caso 1
+
+            case 2: { //Listar las personas
+                System.out.println("Listado de Personas");
+                //Mejorando usando lamda y metodo referencia
+                personas.forEach(System.out::println);
+                break;
+            }
+
+            case 3: { //Salimos del ciclo
+                System.out.println("Hasta pronto...");
+                salir = true;
+            }
+            default:
+                System.out.println("Opcion erronea: " + opcion);
+                break;
+        }//Fin swithc
+        return salir;
+    }
+
+
 }
