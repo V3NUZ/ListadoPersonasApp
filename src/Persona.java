2428e8612ab121f0695daa63b46fe79fe550ba4b
Alejandro <yeisoonc159@gmail.com>
2024-06-13 19:24:32 -0500

    Creamos get y set, junto a los String de padre.

--- src/Persona.java
+++ src/Persona.java
@@ -7,15 +7,61 @@ public class Persona {
 
     // Constructor vacio
     public Persona() {
-        this.id = ++ Persona.numPersonas;
+        this.id = ++Persona.numPersonas;
     }
 
     // Constructor con argumentos
-    public Persona(String nombre, String tel, String email){
+    public Persona(String nombre, String tel, String email) {
         this(); // Se llama el constructor vacio
         this.nombre = nombre;
         this.tel = tel;
         this.email = email;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", tel='" + tel + '\'' +
+                ", email='" + email + '\'' +
+                '}' + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Isabella", "31313", "Sapitabella");
+        System.out.println(persona1);
+    }
 }
